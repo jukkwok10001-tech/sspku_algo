307390a1d89ee105cae9884b2fd0415a9b015f69
jukkwok10001-tech <jukkwok10001@163.com>
2026-01-13 17:36:16 +0800

    Update 快速排序.cpp

--- 第4周_分治策略/oj练习/快速排序/快速排序.cpp
+++ 第4周_分治策略/oj练习/快速排序/快速排序.cpp
@@ -4,12 +4,24 @@ using namespace std;
 int n;
 int a[100010];
 void QuickSort(int l, int r){
-    //1.终止条件
-
-    //2.分(根据元素值划分并递归)
-
-    //3.治(排序方式)
-
+    cout << "l = " << l << "  r = " << r << endl;
+    if(l >= r){
+        return;
+    }
+    int i = l-1; //定义左指针，初始化为l
+    int j = r+1; //定义右指针，初始化为r
+    int pivot = a[l]; //定义待排数组第一个元素的值为基准
+    while(i < j){
+        //只要元素值≤基准值，左指针自增1。i会停在[左起]首个大于基准值的元素
+        do i++; while(a[i] <= pivot); 
+        //只要元素值>基准值，右指针自减1。j会停在[右起]首个小于等于基准值的元素
+        do j--; while(a[j] > pivot);
+        if(i < j){
+            swap(a[i], a[j]);
+        }
+    }
+    QuickSort(l,j);
+    QuickSort(j+1,r);
 }
 int main(){
     cin >> n;
